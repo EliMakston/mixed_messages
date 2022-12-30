5f6c326fa96f1c2faf03f9599e6ad9a4ea2354c8
emakston <epiemuffin@gmail.com>
2022-12-29 22:08:59 -0700

    added random functionality

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 const words = {
     nouns: ['hamster', 'dog', 'cat', 'table', 'lamp', 'bird', 'picture', 'computer', 'mouse', 'rat', 'rhino'],
     adjectives: ['hot', 'angry', 'charming', 'brutal', 'killer', 'dangerous'],
-    verbs: ['running', 'hopping', 'dancing', 'chasing', 'creating', 'following']
+    verbs: ['running', 'hopping', 'dancing', 'chasing', 'creating', 'following'],
+    starters: [1, 2, 3, 4]
+};
+
+const verbs = words.verbs;
+const nouns = words.nouns;
+const adjectives = words.adjectives;
+
+function randomMessage(starter) {
+    switch (starter) {
+        case 1:
+            //TO DO get verb
+            break;
+        case 2:
+            //TO DO get adjective and noun
+            break;
+        case 3:
+            //TO DO get adjective and noun
+            break;
+        case 4:
+            //TO DO get adjective and noun
+            break;
+        default:
+            console.log("Something in the randomMessage() function broke");
+    }
+};
+
+const getRandWord = (type) => {
+    let rand = Math.floor(Math.random() * type.length);
+    return type[rand];
 }
