13bfb9c316ed63f7a01738fb623b7171f8ae7084
emakston <epiemuffin@gmail.com>
2022-12-29 22:18:54 -0700

    completed project

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const words = {
     nouns: ['hamster', 'dog', 'cat', 'table', 'lamp', 'bird', 'picture', 'computer', 'mouse', 'rat', 'rhino'],
-    adjectives: ['hot', 'angry', 'charming', 'brutal', 'killer', 'dangerous'],
+    adjectives: ['hot', 'charming', 'brutal', 'killer', 'dangerous'],
     verbs: ['running', 'hopping', 'dancing', 'chasing', 'creating', 'following'],
     starters: [1, 2, 3, 4]
 };
@@ -13,15 +13,19 @@ function randomMessage(starter) {
     switch (starter) {
         case 1:
             //TO DO get verb
+            return `I foresee ${getRandWord(verbs)} in your future.`
             break;
         case 2:
             //TO DO get adjective and noun
+            return `Soon you will meet a ${getRandWord(adjectives)} ${getRandWord(nouns)}.`
             break;
         case 3:
             //TO DO get adjective and noun
+            return `Beware of the ${getRandWord(adjectives)} ${getRandWord(nouns)}.`
             break;
         case 4:
             //TO DO get adjective and noun
+            return `Only the ${getRandWord(adjectives)} ${getRandWord(nouns)} will know what's in store for you.`
             break;
         default:
             console.log("Something in the randomMessage() function broke");
@@ -32,3 +36,11 @@ const getRandWord = (type) => {
     let rand = Math.floor(Math.random() * type.length);
     return type[rand];
 }
+
+function makeFortune() {
+    console.log(randomMessage(words.starters[Math.floor(Math.random() * words.starters.length)]))
+    console.log(randomMessage(words.starters[Math.floor(Math.random() * words.starters.length)]))
+    console.log(randomMessage(words.starters[Math.floor(Math.random() * words.starters.length)]))
+}
+
+makeFortune();
