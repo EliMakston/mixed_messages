6b0cb860101d51a2211acf6ad67c73e13fcff991
emakston <epiemuffin@gmail.com>
2022-12-29 21:57:16 -0700

    created word bank

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-console.log("Hello world");
+const words = {
+    nouns: ['hamster', 'dog', 'cat', 'table', 'lamp', 'bird', 'picture', 'computer', 'mouse', 'rat', 'rhino'],
+    adjectives: ['hot', 'angry', 'charming', 'brutal', 'killer', 'dangerous'],
+    verbs: ['running', 'hopping', 'dancing', 'chasing', 'creating', 'following']
+}
